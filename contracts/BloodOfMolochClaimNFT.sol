7454accbab5336e9e15b04b48c199d2f5e5bd19c
George <gbh218@gmail.com>
2023-03-04 01:23:18 -0700

    Update claim NFT to basic mint and rename old version with 'old' pretext

--- contracts/BloodOfMolochClaimNFT.sol
+++ contracts/BloodOfMolochClaimNFT.sol
@@ -9,21 +9,19 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "./IBurnable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract BloodOfMolochClaimNFT is
     ERC721URIStorage,
-    EIP712,
     AccessControl,
     IBurnable
 {
     address private PBT_ADDRESS;
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    string private constant SIGNING_DOMAIN = "BloodOfMolochClaimVoucher";
-    string private constant SIGNATURE_VERSION = "1";
 
-    // pre calculated to save gas keccak256("NFTVoucher(uint256 tokenId,string uri,uint256 minPrice)")
-    bytes32 private constant TYPE_HASH =
-        0xc313c44abe7b9471f6e6151a39dadc2293e741d2f166ce98e663dd1a49208461;
+    uint256 public supply;
+    uint256 public constant MAX_SUPPLY = 350;
 
     /// @dev Event to emit on signature mint with the `tokenId`.
     event MintedUsingSignature(uint256 tokenId);
@@ -38,108 +36,22 @@ contract BloodOfMolochClaimNFT is
 
     constructor(address payable minter, address pbtAddress)
         ERC721("Blood of Moloch Claim", "BLMC")
-        EIP712(SIGNING_DOMAIN, SIGNATURE_VERSION)
     {
         require(pbtAddress != address(0), "BloodOfMolochClaimNFT: null address");
         _setupRole(MINTER_ROLE, minter);
         PBT_ADDRESS = pbtAddress;
     }
 
-    /// @notice Represents an un-minted NFT, which has not yet been recorded into the blockchain. A signed voucher can be redeemed for a real NFT using the redeem function.
-    struct NFTVoucher {
-        /// @notice The id of the token to be redeemed. Must be unique - if another token with this ID already exists, the redeem function will revert.
-        uint256 tokenId;
-        /// @notice The metadata URI to associate with this token.
-        string uri;
-        /// @notice The minimum price (in wei) that the NFT creator is willing to accept for the initial sale of this NFT.
-        uint256 minPrice;
+    function mint() public {
+        uint tokenId = supply + 1;
+        supply++;
+        _mint(_msgSender(), tokenId);
     }
 
-    /// @notice Redeems an NFTVoucher for an actual NFT, creating it in the process.
-    /// @param redeemer The address of the account which will receive the NFT upon success.
-    /// @param voucher A signed NFTVoucher that describes the NFT to be redeemed.
-    function redeem(
-        address redeemer,
-        NFTVoucher calldata voucher,
-        bytes calldata signature
-    ) public payable returns (uint256) {
-        // make sure signature is valid and get the address of the signer
-        address signer = _verify(voucher, signature);
-        uint256 tokenId = voucher.tokenId;
-
-        //check that tokenId is within the correct limits
-        require(tokenId < 350, "only 350 claim nft's available");
-
-        //check that token has not already been minted
-        require(
-            minted[tokenId] == false,
-            "cannot claim an already minted token"
-        );
-
-        // make sure that the signer is authorized to mint NFTs
-        require(
-            hasRole(MINTER_ROLE, signer),
-            "Signature invalid or unauthorized"
-        );
-
-        // make sure that the redeemer is paying enough to cover the buyer's cost
-        require(msg.value >= voucher.minPrice, "Insufficient funds to redeem");
-
-        // first assign the token to the signer, to establish provenance on-chain
-        _mint(signer, tokenId);
-        _setTokenURI(tokenId, voucher.uri);
-
-        // transfer the token to the redeemer
-        _transfer(signer, redeemer, tokenId);
-
-        // record payment to signer's withdrawal balance
-        pendingWithdrawals[signer] += msg.value;
-        minted[tokenId] = true;
-
-        emit MintedUsingSignature(tokenId);
-
-        return tokenId;
-    }
-
-    /// @notice Returns a hash of the given NFTVoucher, prepared using EIP712 typed data hashing rules.
-    /// @param voucher An NFTVoucher to hash.
-    function _hash(NFTVoucher calldata voucher)
-        internal
-        view
-        returns (bytes32)
-    {
-        bytes32 STRUCT_HASH = keccak256(
-            abi.encode(
-                TYPE_HASH,
-                voucher.tokenId,
-                keccak256(bytes(voucher.uri)),
-                voucher.minPrice
-            )
-        );
-
-        return _hashTypedDataV4(STRUCT_HASH);
-    }
-
-    /// @notice Verifies the signature for a given NFTVoucher, returning the address of the signer.
-    /// @dev Will revert if the signature is invalid. Does not verify that the signer is authorized to mint NFTs.
-    /// @param voucher An NFTVoucher describing an unminted NFT.
-    function _verify(NFTVoucher calldata voucher, bytes calldata signature)
-        internal
-        view
-        returns (address)
-    {
-        bytes32 digest = _hash(voucher);
-        address recoveredAddress = ECDSA.recover(digest, signature);
-
-        return recoveredAddress;
-    }
-
-    function getChainID() external view returns (uint256) {
-        uint256 id;
-        assembly {
-            id := chainid()
+    function batchMint(uint256 _quantity) external {
+        for(uint i=0; i<_quantity; i++) {
+            mint();
         }
-        return id;
     }
 
     function supportsInterface(bytes4 interfaceId)
@@ -168,6 +80,17 @@ contract BloodOfMolochClaimNFT is
         // zero account before transfer to prevent re-entrancy attack
         pendingWithdrawals[receiver] = 0;
         receiver.transfer(amount);
+    }
+
+    function withdrawTokens(address _token) external {
+        require(
+            hasRole(MINTER_ROLE, msg.sender),
+            "Only authorized minters can withdraw"
+        );
+
+        address receiver = _msgSender();
+        uint balance = IERC20(_token).balanceOf(address(this));
+        IERC20(_token).transfer(receiver, balance);
     }
 
     /// @notice Retuns the amount of Ether available to the caller to withdraw.
