647f20e2682e336be328574454a82d686f02f9b8
Konrad Gnat <konradmgnat@gmail.com>
2023-03-04 22:34:12 -0700

    comment out keyp for now

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -14,58 +14,58 @@ import { useSession, signIn, signOut } from "next-auth/react";
 export default function Home() {
   const { address, isConnected } = useAccount();
   const [_isConnected, _setIsConnected] = useState(false);
-  const { data: session } = useSession();
+  // const { data: session } = useSession();
   const router = useRouter();
 
   useEffect(() => {
     router.push("/babom/claim-pbt");
   }, []);
 
-  useEffect(() => {
-    if (session?.user?.address) {
-      _setIsConnected(true);
-    }
-  }, [session]);
+  // useEffect(() => {
+  //   if (session?.user?.address) {
+  //     _setIsConnected(true);
+  //   }
+  // }, [session]);
 
   // this fixes issue with NextJS: Error: Hydration failed
   useEffect(() => {
     _setIsConnected(isConnected);
   }, [isConnected]);
 
-  const KeypLogin = () => {
-    const LoginButton = ({ provider }) => (
-      <Button
-        m={"2"}
-        color="#EC4899"
-        bg={"#fff"}
-        w={"100%"}
-        maxW={"600px"}
-        onClick={() =>
-          signIn("keyp", null, `login_provider=${provider.toUpperCase()}`)
-        }
-      >
-        {provider}
-      </Button>
-    );
-    if (session?.user?.address)
-      return (
-        <Flex direction={"column"} maxW={"600px"}>
-          <Heading m={"3"} size={"md"}>
-            Connected with Keyp Wallet {session?.user?.address}
-          </Heading>
-        </Flex>
-      );
-    return (
-      <Flex direction={"column"} maxW={"600px"}>
-        <Heading m={"3"} size={"md"}>
-          Create a new wallet
-        </Heading>
-        <LoginButton provider="Discord" />
-        <LoginButton provider="Google" />
-        <LoginButton provider="Chess" />
-      </Flex>
-    );
-  };
+  // const KeypLogin = () => {
+  //   const LoginButton = ({ provider }) => (
+  //     <Button
+  //       m={"2"}
+  //       color="#EC4899"
+  //       bg={"#fff"}
+  //       w={"100%"}
+  //       maxW={"600px"}
+  //       onClick={() =>
+  //         signIn("keyp", null, `login_provider=${provider.toUpperCase()}`)
+  //       }
+  //     >
+  //       {provider}
+  //     </Button>
+  //   );
+  //   if (session?.user?.address)
+  //     return (
+  //       <Flex direction={"column"} maxW={"600px"}>
+  //         <Heading m={"3"} size={"md"}>
+  //           Connected with Keyp Wallet {session?.user?.address}
+  //         </Heading>
+  //       </Flex>
+  //     );
+  //   return (
+  //     <Flex direction={"column"} maxW={"600px"}>
+  //       <Heading m={"3"} size={"md"}>
+  //         Create a new wallet
+  //       </Heading>
+  //       <LoginButton provider="Discord" />
+  //       <LoginButton provider="Google" />
+  //       <LoginButton provider="Chess" />
+  //     </Flex>
+  //   );
+  // };
 
   return (
     <>
@@ -78,7 +78,7 @@ export default function Home() {
             Blood of Moloch NFT
           </Text>
           <Flex direction="column" align="center" justify="center" m={8}>
-            <KeypLogin />
+            {/* <KeypLogin /> */}
             {_isConnected && process.env.NEXT_PUBLIC_DEV_MODE && (
               <DevModePanel />
             )}
