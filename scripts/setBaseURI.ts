41c552c3726ec8a6080e521dc7c9f4336cf27c0d
George <gbh218@gmail.com>
2023-03-17 16:22:53 -0400

    Redeployment setup on goerli

--- scripts/setBaseURI.ts
+++ scripts/setBaseURI.ts
@@ -10,7 +10,7 @@ main().catch((error) => {
 });
 
 async function main() {
-  // URL link for reference: https://nftstorage.link/ipfs/bafybeiapruulwobvtckt7kllbnb6mdijgvm7alivsox5sqdjzksuk5snsy
+  // URL link for reference: https://nftstorage.link/ipfs/bafybeibeqbridcvpivk3dy2tpofxjwcwkj3a732a727yr7roohrn7kwgqy
   const baseURI = "ipfs://bafybeibeqbridcvpivk3dy2tpofxjwcwkj3a732a727yr7roohrn7kwgqy/";
   const contractName = "BloodOfMolochPBT";
 
